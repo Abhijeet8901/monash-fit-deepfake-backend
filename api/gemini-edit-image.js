148d312e0e3d545120cbb7b110c8dfb3db65840f
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-07-10 00:13:17 +1000

    testing 4

--- api/gemini-edit-image.js
+++ api/gemini-edit-image.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI, Modality } from '@google/genai';
+// import { GoogleGenAI, Modality } from '@google/genai';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
